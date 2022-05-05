c6a1282e3d43fa71fe8a82950b082249138453ed
Gocn <hgx233@foxmail.com>
2022-05-05 23:02:52 +0800

    完成多号join的demo，补充工具类中时间格式的反转化

--- src/main/scala/multinum/MultiNum.scala
+++ src/main/scala/multinum/MultiNum.scala
@@ -3,18 +3,24 @@ package multinum
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
+import java.text.SimpleDateFormat
+
 object MultiNum {
+
   def main(args: Array[String]): Unit = {
     //创建一个SparkSession，在Spark2.1中，SparkSession包括了SparkContext，若想使用SparkContext，用sc.sparkContext即可
     val conf = new SparkConf().setMaster("local").setAppName("multi")
     val sc = SparkSession.builder().config(conf).getOrCreate()
-    //创建intRDD
-
+    sc.sparkContext.setLogLevel("WARN")
     // case class Rating(str: String, str1: String, timeS: Int, str2: String)
-
+    val timeFormat = new SimpleDateFormat("yyyyMMddHH")
     val parseRating = (string: String) => {
       val stringArray = string.split(",")
-      (stringArray(0), stringArray(1), stringArray(2).toInt, stringArray(3))
+      lte(stringArray(0),
+        stringArray(1),
+        timeFormat.format(stringArray(2).toInt * 1000),
+        stringArray(2).toInt,
+        stringArray(3))
       // (13800138000,2,1651497821,85931123)
     }
 
@@ -22,14 +28,27 @@ object MultiNum {
 
     val data = sc.sparkContext.textFile("./data/multi/phone.csv")
       .map(parseRating)
-    data.foreach(println)
-    //    data.map(row =>{
-    //      data.row(1)
-    //    }
-    //    )
 
+    data.foreach(println)
+
+    println("==============RESULT===============")
+
+    case class index(subImei: String, event: String)
+
+    val preData = data.map(l => {
+      (index(l.subImei, l.event), l)
+    })
+
+    val finalRes = preData.join(preData)
+      .filter(r => r._2._1.time < r._2._2.time
+        && r._2._2.time - r._2._1.time < 10
+        && r._2._1.num != r._2._2.num)
+      .map(r => (r._1.subImei, r._1.event, r._2._1.time, r._2._2.time, r._2._1.num, r._2._2.num))
+    //      .foreach(println)
+    finalRes.foreach(println)
   }
-  // todo
+
+  case class lte(num: String, event: String, cp: String, time: Long, subImei: String)
 
 }
 
--- src/main/scala/tool/timeformat.scala
+++ src/main/scala/tool/timeformat.scala
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat
 object timeformat {
   def main(args: Array[String]): Unit = {
     val timeFormat = new SimpleDateFormat("yyyyMMddHH")
-    println(timeFormat.parse("2020070719").getTime)
+    val timeStamp = timeFormat.parse("2020070719").getTime //日期转时间戳
+    println(timeStamp)
+    println(timeFormat.format(timeStamp)) //时间戳转日期
   }
 }
