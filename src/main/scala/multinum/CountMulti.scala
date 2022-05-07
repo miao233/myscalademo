43902260e88ec676883b03c8de8113f7b3d8ae14
gocn <hgx233@foxmail.com>
2022-05-07 18:07:05 +0800

    准备号码对统计的类

---
+++ src/main/scala/multinum/CountMulti.scala
@@ -0,0 +1,21 @@
+package multinum
+
+/**
+ * gocn
+ * 2022-5-7
+ */
+
+//todo 号码对统计
+object CountMulti {
+
+  case class muitl(num1:String,
+                   num2:String,
+                   time1:Long,
+                   time2:Long,
+                   event:String,
+                   subImei:String
+                  )
+
+
+
+}
